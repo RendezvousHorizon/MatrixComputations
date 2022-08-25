7996ef23353d77dd9c6ebfed0c8ea0ac9b9593e1
xudong <000690101-17@sjtu.edu.cn>
2022-08-25 19:28:23 +0800

    Add python impl of LU Factorization

---
+++ lu/lu.py
@@ -0,0 +1,184 @@
+from argparse import ArgumentParser
+import timeit
+from typing import Tuple
+import copy
+
+import numpy as np
+
+
+class Solver:
+
+    E = 1e-7
+
+    @staticmethod
+    def assert_ax_eq_b(a, x, b):
+        assert np.max(np.abs(np.matmul(a, x) - b)) < Solver.E
+
+    @staticmethod
+    def assert_a_eq_b(a, b):
+        assert np.max(np.abs(a - b)) < Solver.E
+
+    @staticmethod
+    def solve_lx_eq_b(L: np.matrix, b: np.ndarray, inplace=False) -> np.ndarray:
+        # flops = 2 * [(n - 1) + ... + 1] = n^2 - n
+        old_b = copy.deepcopy(b)
+        if not inplace:
+            b = copy.deepcopy(b)
+        for i in range(min(L.shape)):
+            b[i + 1:] -= L[i + 1:, i] * b[i]
+        Solver.assert_ax_eq_b(L, b, old_b)
+        return b
+
+    @staticmethod
+    def solve(A: np.matrix, b: np.ndarray, lu_method: str, block_size: int) -> np.ndarray:
+        time = timeit.default_timer()
+        LU, L, U = LUFactorator.lu_factorization(A, lu_method, block_size)
+        lu_time = timeit.default_timer() - time
+        m, r, n = *L.shape, U.shape[1]
+        
+        time = timeit.default_timer()
+        # LUx = b -> Ly=b + Ux=y
+        y = np.zeros(r)
+        for i in range(r):
+            y[i] = b[i]
+            b[i + 1:] -= LU[i + 1:, i] * y[i]
+
+        x = np.zeros(r)
+        for j in range(r - 1, -1, -1):
+            x[j] = y[j] / LU[j, j]
+            y[:j] -= LU[:j, j] * x[j]
+        solve_time = timeit.default_timer() - time 
+        print(f'Solve LUx=b time: {solve_time / 1000: .3f}ms')
+
+        return x, round(lu_time / 1e3, 3)
+
+
+class LUFactorator:
+
+    @staticmethod
+    def lu_factorization(A: np.ndarray, lu_method: str, block_size: int=None, inplace=False) -> np.ndarray:
+        if lu_method == 'outer_product':
+            return LUFactorator.lu_factorization_outer_product(A, inplace)
+        if lu_method == 'gaxpy':
+            return LUFactorator.lu_factorization_gaxpy(A, inplace)
+        if lu_method == 'blocked':
+            return LUFactorator.lu_factorization_blocked(A, block_size, inplace)
+        raise ValueError(lu_method)
+
+    @staticmethod
+    def separate_lu(A: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        m, n = A.shape
+        r = min(m, n)
+        L = np.zeros([m, r])
+        U = np.zeros([r, n])
+        for i in range(r):
+            L[i, i] = 1
+            L[i, :i] = A[i, :i]
+            U[i, i:] = A[i, i:]
+        for i in range(r, m):
+            L[i, :] = A[i, :]
+        return L, U
+
+    @staticmethod
+    def lu_factorization_outer_product(A: np.ndarray, inplace=False) -> np.ndarray:
+        # FLOPs 2 / 3 * n^3
+        m, n = A.shape
+        r = min(m, n)
+        A_old = copy.deepcopy(A)
+        if not inplace:
+            A = copy.deepcopy(A)
+        for k in range(r):
+            A[k + 1:, k] /= A[k, k] # FLOPS = (r - k)
+            A[k + 1:, k + 1:] -= np.matmul(A[k + 1:, k:k + 1], A[k:k + 1, k + 1:]) # FLOPS = (r - k)^2
+
+        L, U = LUFactorator.separate_lu(A)
+        Solver.assert_ax_eq_b(L, U, A_old)
+
+        return A, L, U
+
+    @staticmethod
+    def lu_factorization_gaxpy(A: np.matrix, inplace=False) -> np.ndarray:
+        old_A = copy.deepcopy(A)
+        if not inplace:
+            A = copy.deepcopy(A) 
+        A[1:, 0] /= A[0, 0]
+        for j in range(1, min(A.shape)):
+            # solve A[:j,j] = L[:j,:j]U[:j,j] FLOPS j^2
+            for i in range(j + 1):
+                A[i + 1:j + 1, j] -= A[i + 1:j + 1, i] * A[i, j]
+            
+            # calculate L[j+1:,j] FLOPs j * (n - j)
+            A[j+1:, j] = (A[j+1:, j] - np.matmul(A[j+1:, :j], A[:j, j])) / A[j, j]
+
+        L, U = LUFactorator.separate_lu(A)
+
+        Solver.assert_ax_eq_b(L, U, old_A)
+
+        return A, L, U
+
+    @staticmethod
+    def l_inverse(L: np.ndarray) -> np.ndarray:
+        n = L.shape[0]
+        L_inverse = np.identity(n)
+        # TODO 
+        pass 
+
+    @staticmethod
+    def lu_factorization_blocked(A: np.matrix, block_size: int, inplace=False) -> np.ndarray:
+        assert A.shape[0] == A.shape[1]
+        R = block_size
+        submatrix = lambda A, R, i, j: A[i * R:i * R + R, j * R:j * R + R]
+
+        old_A = copy.deepcopy(A)
+        if not inplace:
+            A = copy.deepcopy(A) 
+        M = A.shape[0] // R
+        for km in range(M):
+            k_start = km * R
+            k_end = k_start + R
+            # step 1: lu A[k_start:, k_start:k_end]
+            LUFactorator.lu_factorization_outer_product(A[k_start:, k_start:k_end], inplace=True)
+            # step 2: solve A[k_start:k_end, k_end:] = L[k_start:k_end, k_start:k_end] * U[k_start:k_end, k_end:]
+            Lb = A[k_start:k_end, k_start:k_end]
+            Lb_real = LUFactorator.separate_lu(Lb)[0]
+            for jm in range(km + 1, M):
+                # solve Ab_kmjm = Lb * Ub_kmjm
+                Abkmjm = submatrix(A, R, km, jm)
+                old_Akmjm = submatrix(old_A, R, km, jm)
+                for r in range(R):
+                    Abkmjm[:, r] = Solver.solve_lx_eq_b(Lb_real, Abkmjm[:, r], inplace=False)
+            # step 3: Ab_imjm -= Lbimkm * Ubkmjm
+            for im in range(km + 1, M):
+                for jm in range(km + 1, M):
+                    Abimjm = submatrix(A, R, im, jm)
+                    Abimjm -= np.matmul(submatrix(A, R, im, km), submatrix(A, R, km, jm))
+    
+        L, U = LUFactorator.separate_lu(A)
+
+        Solver.assert_ax_eq_b(L, U, old_A)
+
+        return A, L, U
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--n', type=int, default=1000)
+    parser.add_argument('--r', type=int, default=10)
+    parser.add_argument('--lu_method', choices=['outer_product', 'gaxpy', 'blocked'], type=str, default='outer_product')
+    args = parser.parse_args()
+
+    # initial A, b
+    A = np.random.randn(args.n, args.n)
+    x_correct = np.random.randn(args.n)
+    b = np.matmul(A, x_correct)
+
+    x, dur = Solver.solve(A, b, args.lu_method, args.r)
+
+    Solver.assert_a_eq_b(x, x_correct)
+
+    block_size_str = f' r={args.r}' if args.lu_method == 'blocked' else ''
+    print(f'N={args.n}, {args.lu_method}{block_size_str} time={dur}ms')
+
+
+if __name__ == '__main__':
+    main()
